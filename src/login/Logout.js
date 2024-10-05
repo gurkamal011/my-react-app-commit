33137dac77360e7f4a1ffe180f2ad64e50141757
gurkamal011 <singhgurkamal66@gmail.com>
2024-10-05 17:04:31 +0530

    05-10-2024 commit

---
+++ src/login/Logout.js
@@ -0,0 +1,18 @@
+// src/components/Logout.js
+import React from 'react';
+
+const Logout = () => {
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        window.location.href = '/';
+    };
+
+    return (
+        <div>
+            <p>Logging out...</p>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    );
+};
+
+export default Logout;
